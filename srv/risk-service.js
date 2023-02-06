5809ff2101d747450c667c9a8dacaf184a8c533c
czhunor <czhunor@gmail.com>
2023-02-06 16:04:37 +0100

    Add External Service to the project

--- srv/risk-service.js
+++ srv/risk-service.js
@@ -1,18 +1,23 @@
-
-const cds = require('@sap/cds')
+const cds = require("@sap/cds");
 
 /**
  * Implementation for Risk Management service defined in ./risk-service.cds
  */
-module.exports = cds.service.impl(async function() {
-    this.after('READ', 'Risks', risksData => {
-        const risks = Array.isArray(risksData) ? risksData : [risksData];
-        risks.forEach(risk => {
-            if (risk.impact >= 100000) {
-                risk.criticality = 1;
-            } else {
-                risk.criticality = 2;
-            }
-        });
+module.exports = cds.service.impl(async function () {
+  const bupa = await cds.connect.to("API_BUSINESS_PARTNER");
+
+  this.on("READ", "Suppliers", async (req) => {
+    return bupa.run(req.query);
+  });
+
+  this.after("READ", "Risks", (risksData) => {
+    const risks = Array.isArray(risksData) ? risksData : [risksData];
+    risks.forEach((risk) => {
+      if (risk.impact >= 100000) {
+        risk.criticality = 1;
+      } else {
+        risk.criticality = 2;
+      }
     });
+  });
 });
